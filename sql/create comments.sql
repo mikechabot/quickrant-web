0297fda15bc3d3400adef2b3013c82837483b07e
Mike Chabot <mchabot@gmail.com>
2013-07-20 11:00:08 -0400

    adding sql

---
+++ sql/create comments.sql
@@ -0,0 +1,4 @@
+﻿drop table if exists comments;
+
+
+create table comments (id serial unique, created timestamp (60), emotion varchar(16), comment varchar(500), commenter varchar(100), location varchar(150));
