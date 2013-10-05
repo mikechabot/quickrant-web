4c583d2c9b15f1c5f6a7ef71ed9d639d899a2d80
Mike Chabot <mchabot@gmail.com>
2013-10-04 22:35:06 -0400

    committing updated sql

--- sql/create comments.sql
+++ sql/create comments.sql
@@ -2,4 +2,4 @@ drop table if exists rants;
 drop table if exists cookies;
 
 create table rants (id serial unique, created timestamp (60), emotion varchar(16), question varchar(500), rant varchar(500), ranter varchar(100), location varchar(150));
-create table cookies (id serial unique, created timestamp (60), issued varchar (60), cookie varchar(50))
+create table cookies (id serial unique, created timestamp (60), issued bigint, cookie varchar(50));
