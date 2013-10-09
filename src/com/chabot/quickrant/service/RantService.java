0a6086e7adfec14a15e23479798f1b1a2e85887d
Mike Chabot <mchabot@gmail.com>
2013-10-08 22:14:06 -0400

    lastrant time now recorded when posting, will be used in rate limiting soon

--- src/com/chabot/quickrant/service/RantService.java
+++ src/com/chabot/quickrant/service/RantService.java
@@ -5,8 +5,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import com.chabot.quickrant.Configuration;
+import com.chabot.quickrant.Configuration.ConfigurationException;
 import com.chabot.quickrant.Params;
 import com.chabot.quickrant.database.Database;
 import com.chabot.quickrant.model.Rant;
@@ -14,13 +19,24 @@ import com.chabot.quickrant.utils.DateUtils;
 
 public class RantService {
 	
+	private static Logger log = Logger.getLogger(RantService.class);
+	
 	public static void createRant(Rant rant, Params params) throws SQLException {				
 		
+    	Configuration config = Configuration.getInstance();
+		try {
+			config.initialize();
+		} catch (ConfigurationException e) {
+			log.error("Unable to load configuration", e);
+		}
+		
 		Connection connection =  new Database().getConnection();
-	    String selectSql = "select id from ranter where cookievalue = '" + params.getCookieValue(CookieService.COOKIE_NAME) + "';";
+	    String selectSql = "select id, rantattempts, lastrant from ranter where cookievalue = '" + params.getCookieValue(CookieService.COOKIE_NAME) + "';";
 		String insertSql = "insert into rants (id, created, emotion, question, rant, ranter, location, ranter_id) values (nextval('rants_id_seq'),?,?,?,?,?,?,?);";		    
+		String updateSql = "update ranter set lastrant = " + new Date().getTime() +" where id = %id%";
 		PreparedStatement selectStatement = connection.prepareStatement(selectSql);
-		PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
+		PreparedStatement insertStatement = connection.prepareStatement(insertSql);
+		PreparedStatement updateStatement = null;
 	    
 	    if(rant.getRanter() == null || rant.getRanter().trim().length() == 0) {
 	    	 rant.setRanter("Anonymous");
@@ -29,21 +45,25 @@ public class RantService {
 	    	 rant.setLocation("Earth");
 	    }		    
 	    
-	    int id = 0;
+	    int id = 0;	    
 	    ResultSet rs = selectStatement.executeQuery();
-	    if (rs.next()) id = rs.getInt(1);
+	    if (rs.next()) id = rs.getInt(1);	    	
 	    	    
-	    preparedStatement.setTimestamp(1, DateUtils.getCurrentTimeStamp());
-	    preparedStatement.setString(2,  rant.getEmotion());
-	    preparedStatement.setString(3,  rant.getQuestion());
-	    preparedStatement.setString(4,  rant.getRant());
-	    preparedStatement.setString(5,  rant.getRanter());
-	    preparedStatement.setString(6,  rant.getLocation());
-	    preparedStatement.setInt(7, id);
-	    preparedStatement.executeUpdate();
+	    insertStatement.setTimestamp(1, DateUtils.getCurrentTimeStamp());
+	    insertStatement.setString(2,  rant.getEmotion());
+	    insertStatement.setString(3,  rant.getQuestion());
+	    insertStatement.setString(4,  rant.getRant());
+	    insertStatement.setString(5,  rant.getRanter());
+	    insertStatement.setString(6,  rant.getLocation());
+	    insertStatement.setInt(7, id);
+	    insertStatement.executeUpdate();
+	    
+	    updateStatement = connection.prepareStatement(updateSql.replace("%id%", String.valueOf(id)));
+	    updateStatement.execute();
 
-		if (preparedStatement != null) preparedStatement.close();
+		if (insertStatement != null) insertStatement.close();
 		if (selectStatement != null) selectStatement.close();
+		if (updateStatement != null) updateStatement.close();
 		if (connection != null) connection.close();
 	}
 	
