f1121895bf7758c33fa2277ed7395ab2ddbc1f8e
Mike <mike.chabot@live.com>
2015-10-05 21:44:21 -0400

    pulling some html out of the rant directive

--- src/main/webapp/js/angular_qr.js
+++ src/main/webapp/js/angular_qr.js
@@ -1,15 +1,23 @@
 var app = angular.module('quickrant', ['ui.bootstrap', 'ngAnimate', 'ngTimeago']);
 
-app.controller('MainController', ['$scope', '$timeout', 'QR_DATA', 'QR_CONST', 'QuickrantFactory', function ($scope, $timeout, QR_DATA, QR_CONST, QuickrantFactory) {
+app.controller('MainController', ['$scope', '$timeout', 'QR_DATA', 'QR_CONST', 'QuickrantFactory', 'DialogService', function ($scope, $timeout, QR_DATA, QR_CONST, QuickrantFactory, DialogService) {
 
     var _initQuickrant = function() {
 
         $scope.quickrant = {};
 
+        $scope.showShare = function() {
+            DialogService.open({
+                templateUrl: '/templates/modals/share.html',
+                scope: QR_DATA.shareUrls
+            });
+        };
+
         QuickrantFactory()
             .done(function(quickrant) {
                 $timeout(function() {
                     $scope.quickrant = quickrant;
+                    $scope.page = $scope.quickrant.page;
                 });
             });
 
