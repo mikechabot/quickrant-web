4ff504fe7bad7f2d92925d287da696c846a06bb5
Mike <mike.chabot@live.com>
2015-11-26 03:22:02 -0500

    adding some sections, updating directives, need to move shit to a svc

--- src/main/webapp/js/angular_quickrant.js
+++ src/main/webapp/js/angular_quickrant.js
@@ -1,18 +1,28 @@
-var app = angular.module('quickrant', ['ngRoute', 'yaru22.angular-timeago']);
+var app = angular.module('quickrant', ['ngRoute', 'ngAnimate', 'yaru22.angular-timeago']);
 
 app.config(function($routeProvider) {
     $routeProvider
         .when('/', {
-            template : '<introduction></introduction>'
+            template : '<intro-section></intro-section>'
         })
         .when('/questions', {
-            template : '<questions></questions>'
+            template : '<question-section></question-section>'
         })
+        .when('/rant', {
+            template: '<rant-form-section></rant-form-section>'
+        })
+        .when('blank', {
+            template: ''
+        });
 });
 
-
-
-
-
+app.filter('decapitalize', function() {
+    return function(input) {
+        if (angular.isString(input) && angular.hasValue(input)) {
+            return input.charAt(0).toLowerCase() + input.slice(1);
+        }
+        return input;
+    }
+});
 
 
--- src/main/webapp/js/directive/directives.js
+++ src/main/webapp/js/directive/directives.js
@@ -6,6 +6,15 @@ app.directive('application', ['APP_DATA', function (APP_DATA) {
             this.setEmotion = function(emotion) {
                 $scope.emotion = emotion;
             };
+            this.getEmotion = function() {
+                return $scope.emotion;
+            };
+            this.setQuestion = function(question) {
+                $scope.question = question;
+            };
+            this.getQuestion = function() {
+                return $scope.question;
+            };
             this.getEmotions = function() {
                 return APP_DATA.EMOTIONS;
             };
@@ -18,19 +27,25 @@ app.directive('application', ['APP_DATA', function (APP_DATA) {
     }
 }]);
 
-app.directive('introduction', function () {
+app.directive('introSection', function () {
     var templateHtml =
-        '<div class="section hero">' +
+        '<div class="section intro">' +
             '<div class="container">' +
                 '<div class="row">' +
                     '<div class="one-half column">' +
-                        '<div>' +
-                            '<h4 class="hero-heading">How are you feeling today?</h4>' +
+                        '<h4 class="intro-heading">How are you feeling today?</h4>' +
+                        '<div class="mobile-toggle-lg decline-rant-lg">' +
+                            '<i class="fa fa-lg fa-hand-o-right"></i>&nbsp;&nbsp;<a href="#blank">None of your business.</a>' +
                         '</div>' +
                     '</div>' +
                     '<div class="one-half column">' +
                         '<a ng-repeat="emotion in emotions" href="#questions" class="facial-emotion" ng-click="setEmotion(emotion)"><img ng-src="/images/{{emotion.name}}.gif"></a>' +
                     '</div>'+
+                '</div>' +
+                '<div class="row mobile-toggle-sm decline-rant-sm">' +
+                    '<div class="twelve columns">' +
+                        '<i class="fa fa-lg fa-hand-o-right"></i>&nbsp;&nbsp;<a href="#blank">None of your business.</a>' +
+                    '</div>' +
                 '</div>' +
             '</div>'+
         '</div>';
@@ -49,18 +64,27 @@ app.directive('introduction', function () {
 });
 
 
-app.directive('questions', function () {
+app.directive('questionSection', function () {
 
     var templateHtml =
         '<div class="section questions">' +
             '<div class="container">' +
-                '<h4>I feel {{emotion.name}} right now, and...</h4>' +
+                '<div class="row">' +
+                    '<div class="twelve columns">' +
+                        '<h4>I feel <span class="bold-400">{{emotion.name}}</span> right now, and...</h4>' +
+                    '</div>' +
+                '</div>' +
                 '<div class="row">' +
                     '<div class="two columns">' +
-                        '<a href="/#/" class="facial-emotion"><img ng-src="/images/{{emotion.name}}.gif"></a><h5></h5>' +
+                        '<a href="/#/" class="facial-emotion"><img ng-src="/images/{{emotion.name}}.gif"></a>' +
                     '</div>' +
                     '<div class="ten columns">' +
-                        '<button class="question" ng-repeat="question in questions">{{question}}</button>' +
+                        '<a href="#rant"><button class="question" ng-repeat="question in questions" ng-click="setQuestion(question)">{{question}}</button></a>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="row back-to-intro">' +
+                    '<div class="twelve columns">' +
+                        '<i class="fa fa-lg fa-hand-o-right"></i>&nbsp;&nbsp;<a href="/#/">Wait, I don\'t feel {{emotion.name}}.</a>' +
                     '</div>' +
                 '</div>' +
             '</div>' +
@@ -75,6 +99,51 @@ app.directive('questions', function () {
             scope.setQuestion = function(question) {
                 AppCtrl.setQuestion(question);
             }
+        }
+    }
+});
+
+app.directive('rantFormSection', function () {
+
+    var templateHtml =
+        '<div class="section rant-form-section">' +
+            '<div class="container">' +
+                '<div class="row">' +
+                    '<div class="twelve columns">' +
+                        '<h4>I feel <span class="bold-400">{{emotion.name}}</span> right now, and <em>{{question | decapitalize}}</em></h4>' +
+                    '</div>' +
+                '</div>' +
+                '<form name="form">' +
+                    '<div class="row">' +
+                        '<div class="one-half column">' +
+                            '<input class="u-full-width" type="text" placeholder="Name (Optional)" id="name">' +
+                        '</div>' +
+                        '<div class="one-half column">' +
+                            '<input class="u-full-width" type="text" placeholder="Location (Optional)" id="location">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<div class="twelve columns">' +
+                            '<textarea class="u-full-width" placeholder="Say something..." id="rant"></textarea>' +
+                        '</div>' +
+                    '</div>' +
+                    '<input type="submit" value="Submit">' +
+                '</form>' +
+                '<div class="row back-to-intro">' +
+                    '<div class="twelve columns">' +
+                        '<i class="fa fa-lg fa-hand-o-right"></i>&nbsp;&nbsp;<a href="#questions">Wait, I don\'t feel this way.</a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+
+    return {
+        restrict: 'EA',
+        require: '^application',
+        template: templateHtml,
+        link: function (scope, element, attrs, AppCtrl) {
+            scope.emotion = AppCtrl.getEmotion();
+            scope.question = AppCtrl.getQuestion();
         }
     }
 });
